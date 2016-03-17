110e4978d4db519c6864c51963ce3b9abb730f85
nazda <nazda@gmx.com>
2016-03-17 11:18:17 +0300

    Update optitrackers.user.js

--- optitrackers.user.js
+++ optitrackers.user.js
@@ -10,94 +10,45 @@
 // @match          http://pornolab.net/*
 // @match          http://rutracker.org/*
 // @icon           http://i.imgur.com/vCfrH6c.png
-// ==/UserScript==
-var e = document.getElementById("tor-hash");
-if (e) {
-    var hash = e.innerText;
-    if (/^[0-9A-F]{40}$/.test(hash)) {
-       e.innerHTML = "<a href='magnet:?xt=urn:btih:" + hash + "&tr=http%3A%2F%2Fbt2.rutracker.cc%2Fann%3Fmagnet&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969'>" + hash + "</a>";
-    }
-}
-
-var vr = document.getElementsByClassName("post_btn_2")[0];
-vr.src = i.src.replace('./profile.php?mode=viewprofile&amp;u=', '');
-alert(vr);
 
 if(window.location.href.match(/\/forum\/tracker*/i))
 	tracker();
 else if(window.location.href.match(/\/forum\/viewforum*/i))
 	viewforum();
 else if(window.location.href.match(/\/forum\/viewtopic*/i))
-	viewtopic();
-function tracker() {
-
-//	alert("tracker");
-}
-function viewforum() {
-
-//	alert("viewforum");
-      var e = document.getElementById("search-f-form");
-      if (e) {
-	 // var hash = e.innerText;
-	 // alert(e);
-	 	e.innerHTML = "<a href='magnet:?xt=urn:btih:hhhh"'>jghjhgjhjh</a>";
-
-     }
+	viewtopic()
+else if(window.location.href.match(/\/forum\/search*/i))
+	search();
+function search() {
+  //GM_xmlhttpRequest();
 }
 function viewtopic() {
-	//tor-hash
-      var e = document.getElementById("tor-hash");
-      if (e) {
-	  var hash = e.innerText;
-	 if (/^[0-9A-F]{40}$/.test(hash)) {
-		e.innerHTML = "<a href='magnet:?xt=urn:btih:" + hash + "'>" + hash + "</a>";
-	  }
-     }
+  
+  var v = document.getElementsByTagName('tbody');
+    for (var i = 0; i < v.length; i++) {
+        if((v[i].className === 'row1') || (v[i].className === 'row2'))
+          var info = v[i].getElementsByClassName("poster_btn td3")[0];
+      if (info) {
+  var Dinfo = info.getElementsByTagName('div')[0];
+  if (Dinfo) {
+    var TDinfo = Dinfo.innerHTML;
+    var Tinfo = Dinfo.getElementsByTagName('a')[0].href;
+    ID = Tinfo.replace('http://pornolab.net/forum/profile.php?mode=viewprofile&u=', '');
+    var profileurl = "<a class='txtb' href='/forum/profile.php?mode=viewprofile&u=" + ID + "'>[Profile]</a>&nbsp;";
+    profileurl = profileurl +  "<a class='txtb' href='/forum/search.php?search_author=1&uid=" + ID + "'>[Search]</a>";
+    Dinfo.innerHTML = profileurl;
+ }
 }
+      
+          var info = v[i].getElementsByClassName("poster_info td1")[0];
+         if (info) {
+  var Dinfo = info.getElementsByTagName('a')[0].name;
+  if (Dinfo) {
+    var Dinfo = info.getElementsByTagName('p')[0];
+   // alert(profileurl);
+    Dinfo.innerHTML = profileurl;
+  }
 
-//load jQuery (if you know the way to getJSON with callback and without jQuery, please, contact me).
-function addJQuery(callback) {
-  var script = document.createElement("script");
-  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-}, false);
-  document.body.appendChild(script);
-}
-
-function changeScreenshots() {
-    function formYQLRequest (url) {
-        return "http://query.yahooapis.com/v1/public/yql?q=select%20src%20from%20html%20where%20url%3D%22"+
-               encodeURIComponent(url) + "%22%20AND%20xpath%3D%22%2F%2Fimg%5B%40onclick%3D'scale(this)%3B'%5D%22&format=xml'&callback=?"
-    }
-
-    var vars = document.getElementsByTagName('var');
-    var fastpicRE = /^(http:\/\/.*\.fastpic\.ru\/)thumb(.*)(jpe?g)$/;
-    var imagebamRE = /imagebam\.com/;
-    var matches;
-    
-    for (var i = 0; i < vars.length; i++) {
-        if (vars[i].className === 'postImg') {
-            //Fastpic screenshots
-            if (matches = vars[i].title.match(fastpicRE)) {
-                vars[i].title = matches[1] + 'big' + matches[2] + 'jpg';
-            }
-            //ImageBam screenshots
-            else if (vars[i].title.match(imagebamRE)) {
-                $.getJSON(formYQLRequest(vars[i].parentNode.href),
-                          (function() {
-                              var image = vars[i];
-                              return function(data){ image.title = data.results[0].match(/src="([^"]+)/)[1]; }
-                          })()
-                         );
-            }
         }
     }
-
-    //Add max-width to pictures.
-    var sheet = document.styleSheets[document.styleSheets.length - 1];
-    sheet.insertRule('img.postImg { max-width:' + (window.innerWidth - 250) + 'px !important; height: auto !important; }', sheet.cssRules.length);
 }
-addJQuery(changeScreenshots);
