314700e2ea0b85c105396d49e73d082a8c3ab68c
nazda <nazda@gmx.com>
2016-03-17 12:03:06 +0300

    Update optitrackers.user.js

--- optitrackers.user.js
+++ optitrackers.user.js
@@ -11,44 +11,49 @@
 // @match          http://rutracker.org/*
 // @icon           http://i.imgur.com/vCfrH6c.png
 
-if(window.location.href.match(/\/forum\/tracker*/i))
-	tracker();
-else if(window.location.href.match(/\/forum\/viewforum*/i))
-	viewforum();
-else if(window.location.href.match(/\/forum\/viewtopic*/i))
-	viewtopic();
-else if(window.location.href.match(/\/forum\/search*/i))
-	search();
-function search() {
-  //GM_xmlhttpRequest();
-}
-function viewtopic() {
-  
-  var v = document.getElementsByTagName('tbody');
-    for (var i = 0; i < v.length; i++) {
-        if((v[i].className === 'row1') || (v[i].className === 'row2'))
-          var info = v[i].getElementsByClassName("poster_btn td3")[0];
-      if (info) {
-  var Dinfo = info.getElementsByTagName('div')[0];
-  if (Dinfo) {
-    var TDinfo = Dinfo.innerHTML;
-    var Tinfo = Dinfo.getElementsByTagName('a')[0].href;
-    ID = Tinfo.replace('http://pornolab.net/forum/profile.php?mode=viewprofile&u=', '');
-    var profileurl = "<a class='txtb' href='/forum/profile.php?mode=viewprofile&u=" + ID + "'>[Profile]</a>&nbsp;";
-    profileurl = profileurl +  "<a class='txtb' href='/forum/search.php?search_author=1&uid=" + ID + "'>[Search]</a>";
-    Dinfo.innerHTML = profileurl;
- }
-}
-      
-          var info = v[i].getElementsByClassName("poster_info td1")[0];
-         if (info) {
-  var Dinfo = info.getElementsByTagName('a')[0].name;
-  if (Dinfo) {
-    var Dinfo = info.getElementsByTagName('p')[0];
-   // alert(profileurl);
-    Dinfo.innerHTML = profileurl;
-  }
+(function() {
+    if (window.location.href.match(/\/forum\/tracker*/i))
+        tracker();
+    else if (window.location.href.match(/\/forum\/viewforum*/i))
+        viewforum();
+    else if (window.location.href.match(/\/forum\/viewtopic*/i))
+        viewtopic();
+    else if (window.location.href.match(/\/forum\/search*/i))
+        search();
 
-        }
+    function search() {
+        //GM_xmlhttpRequest();
     }
-}
+
+    function viewtopic() {
+        var info, Dinfo, profileurl;
+        var v = document.getElementsByTagName('tbody');
+        for (var i = 0; i < v.length; i++) {
+            if ((v[i].className === 'row1') || (v[i].className === 'row2'))
+				info = v[i].getElementsByClassName('poster_btn td3')[0];
+            if (info){
+                Dinfo = info.getElementsByTagName('div')[0];
+                if (Dinfo) {
+                    var TDinfo = Dinfo.innerHTML;
+                    var Tinfo = Dinfo.getElementsByTagName('a')[0].href;
+                    ID = Tinfo.replace('http://pornolab.net/forum/profile.php?mode=viewprofile&u=', '');
+                    profileurl = "<a class='txtb' href='/forum/profile.php?mode=viewprofile&u=" + ID + "'>[Profile]</a>&nbsp;";
+                    profileurl = profileurl + "<a class='txtb' href='/forum/search.php?search_author=1&uid=" + ID + "'>[Search]</a>";
+                    Dinfo.innerHTML = profileurl;
+                }
+            }
+
+            info = v[i].getElementsByClassName("poster_info td1")[0];
+            if (info) {
+                Dinfo = info.getElementsByTagName('a')[0].name;
+                if (Dinfo) {
+                    Dinfo = info.getElementsByTagName('p')[0];
+                    // alert(profileurl);
+                    Dinfo.innerHTML = profileurl;
+                }
+
+            }
+        }
+
+    }
+})();
