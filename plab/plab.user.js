69f7ed517de4a64946ddfbb543f87818ee93706b
nazda <nazda@gmx.com>
2017-03-20 11:49:01 +0300

    Update plab.user.js

--- plab/plab.user.js
+++ plab/plab.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/nazda/js/raw/master/plab/plab.user.js
 // @description  Plab core
 // @author       nazda
-// @version        2017.03.20 5
+// @version        2017.03.20 6
 // @updateURL      https://github.com/nazda/js/raw/master/plab/plab.user.js
 // @downloadURL    https://github.com/nazda/js/raw/master/plab/plab.user.js
 // @homepage       https://github.com/nazda/js/plab
