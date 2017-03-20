1200aa85805ed0298665a0b3f0d3f82e93eac001
nazda <nazda@gmx.com>
2017-03-20 06:43:02 +0300

    Update nazda.pornolab_sort.user.js

--- nazda.pornolab_sort.user.js
+++ nazda.pornolab_sort.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/nazda/js/blob/master/nazda.pornolab.user.js
 // @description  Sort
 // @author       nazda
-// @version        2017.03.20 1
+// @version        2017.03.20 2
 // @updateURL      https://raw.githubusercontent.com/nazda/js/master/nazda.pornolab_sort.user.js
 // @downloadURL    https://raw.githubusercontent.com/nazda/js/master/nazda.pornolab_sort.user.js
 // @homepage       https://github.com/nazda/js
@@ -12,6 +12,7 @@
 // @icon           http://i.imgur.com/vCfrH6c.png
 // ==/UserScript==
 (function() {
+/*
 	var search = document.getElementById("o");
 for (var i = 0; i < search.length; i++) {
 	if (search.options[i].value == "10") {
@@ -19,11 +20,11 @@ for (var i = 0; i < search.length; i++) {
 		break;
 	}
 }
-/*
+*/
 if (!sessionStorage.getItem('changed')) {
 	document.getElementById('o').value = 10;
 	sessionStorage.setItem('changed', 'true');
 	document.getElementById('tr-form').submit();
 }
-*/
+
 })();
